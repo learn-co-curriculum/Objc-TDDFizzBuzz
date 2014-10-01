ad722a70c3fe58a10f75badcbd3cc14daf987702
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-10-01 18:13:13 -0400

    cleanup- add solution

--- Objc-TDDFizzBuzz/FizzBuzz.h
+++ Objc-TDDFizzBuzz/FizzBuzz.h
@@ -1,19 +1,27 @@
 //
 //  FizzBuzz.h
-//  Objc-TDDFizzBuzz
+//  TDD FizzBuzz
 //
-//  Created by Al Tyus on 6/3/14.
-//  Copyright (c) 2014 Flatiron School. All rights reserved.
+//  Created by Al Tyus on 1/16/14.
+//  Copyright (c) 2014 Al Tyus. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface FizzBuzz : NSObject
 
-@property (nonatomic) NSNumber *startNumber;
-@property (nonatomic) NSNumber *endNumber;
+@property (strong, nonatomic) NSNumber *startNumber;
+@property (strong, nonatomic) NSNumber *endNumber;
 
-- (instancetype)init;
-- (instancetype)initWithStartNumber:(NSNumber *)startNumber endNumber:(NSNumber *)endNumber;
+- (id)init;
+- (instancetype)initWithStartNumber:(NSNumber *)startNumber
+                          endNumber:(NSNumber *)endNumber;
+
+- (NSArray *)getFizzBuzzArray;
++ (NSArray *)getValuesBetweenNumber:(NSNumber *)startNumber
+                          endNumber:(NSNumber *)endNumber;
+
++ (instancetype)fizzBuzzWithStartNumber:(NSNumber *)startNumber
+                              endNumber:(NSNumber *)endNumber;
 
 @end
--- Objc-TDDFizzBuzz/FizzBuzz.m
+++ Objc-TDDFizzBuzz/FizzBuzz.m
@@ -1,13 +1,83 @@
 //
 //  FizzBuzz.m
-//  Objc-TDDFizzBuzz
+//  TDD FizzBuzz
 //
-//  Created by Al Tyus on 6/3/14.
-//  Copyright (c) 2014 Flatiron School. All rights reserved.
+//  Created by Al Tyus on 1/16/14.
+//  Copyright (c) 2014 Al Tyus. All rights reserved.
 //
 
 #import "FizzBuzz.h"
 
 @implementation FizzBuzz
+
+- (id)init
+{
+    self = [super init];
+    if (self)
+    {
+        self = [self initWithStartNumber:@0
+                               endNumber:@0];
+    }
+    return self;
+}
+- (instancetype)initWithStartNumber:(NSNumber *)startNumber
+                          endNumber:(NSNumber *)endNumber
+{
+    self = [super init];
+    if (self)
+    {
+        _startNumber = startNumber;
+        _endNumber = endNumber;
+    }
+    return self;
+}
+
+- (NSArray *)getFizzBuzzArray
+{
+    return [FizzBuzz getValuesBetweenNumber:self.startNumber
+                                  endNumber:self.endNumber];
+}
+
++ (instancetype)fizzBuzzWithStartNumber:(NSNumber *)startNumber
+                              endNumber:(NSNumber *)endNumber
+{
+    return [[FizzBuzz alloc] initWithStartNumber:startNumber
+                                       endNumber:endNumber];
+}
++ (NSArray *)getValuesBetweenNumber:(NSNumber *)startNumber
+                          endNumber:(NSNumber *)endNumber
+{
+    NSMutableArray *fizzBuzz = [[NSMutableArray alloc] init];
+    
+    NSInteger count = [startNumber integerValue];
+    NSInteger endInt = [endNumber integerValue];
+    
+    for (NSInteger x = count; x <= endInt; x++)
+    {
+        if (x == 0)
+        {
+            [fizzBuzz addObject:[@(x) description]];
+        }
+        else if (x % 3 == 0 && x % 5 == 0)
+        {
+            [fizzBuzz addObject:@"FizzBuzz"];
+        }
+        else if (x % 3 == 0)
+        {
+            [fizzBuzz addObject:@"Fizz"];
+        }
+        else if (x % 5 == 0)
+        {
+            [fizzBuzz addObject:@"Buzz"];
+        }
+        else
+        {
+            [fizzBuzz addObject:[@(x) description]];
+        }
+    }
+    NSLog(@"%@", fizzBuzz);
+    
+    return [NSArray arrayWithArray:fizzBuzz];
+}
 
 @end
