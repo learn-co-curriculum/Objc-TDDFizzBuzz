1ca671be8cf57dfd24ec3524c388f988469a8bc9
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-10-01 18:14:37 -0400

    cleanup- update tests

--- Objc-TDDFizzBuzzTests/FizzBuzzTests.m
+++ Objc-TDDFizzBuzzTests/FizzBuzzTests.m
@@ -1,4 +1,5 @@
 #import "Specta.h"
+
 #define EXP_SHORTHAND
 #import "Expecta.h"
 #import "FizzBuzz.h"
@@ -31,26 +32,38 @@ describe(@"Fizz Buzz Tests", ^{
 #pragma mark - Test 2
     it(@"Should allow it's startNumber property to be set", ^{
         expect(defaultFizzBuzz).to.respondTo(@selector(setStartNumber:));
+        defaultFizzBuzz.startNumber = @10;
+        expect(defaultFizzBuzz.startNumber).to.equal(@10);
     });
     
 #pragma mark - Test 3
     it(@"Should allow it's endNumber property to be set", ^{
-
+        expect(defaultFizzBuzz).to.respondTo(@selector(setEndNumber:));
+        defaultFizzBuzz.endNumber = @20;
+        expect(defaultFizzBuzz.endNumber).to.equal(@20);
     });
     
 #pragma mark - Test 4
     it(@"Should set startNumber and endNumber to @0 when initialized using default initializer", ^{
-
+        expect(defaultFizzBuzz.startNumber).to.equal(@0);
+        expect(defaultFizzBuzz.endNumber).to.equal(@0);
     });
     
 #pragma mark - Test 5
     it(@"Should return a FizzBuzz Object when initialized with a designated initializer", ^{
-
+        expect(designatedFizzBuzz).to.beKindOf([FizzBuzz class]);
+        expect(designatedFizzBuzz).toNot.beNil();
+        
+        expect(designatedFizzBuzz.startNumber).to.equal(@0);
+        expect(designatedFizzBuzz.endNumber).to.equal(@15);
     });
     
 #pragma mark - Test 6
     it(@"Should replace appropriate values with Fizz, Buzz, and FizzBuzz", ^{
+        
+        NSArray *fizzBuzzArray = @[@"0",@"1",@"2",@"Fizz",@"4",@"Buzz",@"Fizz",@"7",@"8",@"Fizz",@"Buzz", @"11", @"Fizz", @"13", @"14", @"FizzBuzz"];
 
+        expect([designatedFizzBuzz getFizzBuzzArray]).to.equal(fizzBuzzArray);
     });
     
     afterEach(^{
